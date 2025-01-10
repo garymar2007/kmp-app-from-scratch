3499faceef544dd954bf329a5e3a70607d227b0d
garyma <garymar2007@gmail.com>
2025-01-10 11:49:19 +0200

    Added sourceSets for commonMain dependencies and simple MainScreen composable function.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,15 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+
+    sourceSets.commonMain.dependencies {
+        implementation(compose.runtime)
+        implementation(compose.foundation)
+        implementation(compose.material3)
+    }
 }
 
 android {
     compileSdk = 33
-    namespace = "com.gary.app"
+    namespace = "com.gary.kotlinfromscratch"
 }
---
+++ app/src/commonMain/kotlin/com/gary/kotlinfromscratch/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.gary.kotlinfromscratch
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainScreen() {
+        Text("Hello World", fontSize = 42.sp)
+}
